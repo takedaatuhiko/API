a5e1fa51dee2cb1179cec682dd19368e7783f08c
takedaatuhiko <singen0219@gmail.com>
2019-02-16 15:24:14 +0900

    firstcommit

--- sample2/main.mjs
+++ sample2/main.mjs
@@ -25,7 +25,7 @@
  */
 async function requestGeoCoderAPI(query) {
   const response = await axios({
-    url: 'https://{あなたのApigeeURL}/geocode/V1/geoCoder',
+    url: 'https://singen0219-eval-test.apigee.net/yahoo_map/geocode/V1/geoCoder',
     params: {
       query,
       output: 'json',
