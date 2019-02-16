63ca3eddb8ed1b34da735c83ee207b0540b466c6
takedaatuhiko <singen0219@gmail.com>
2019-02-16 16:40:25 +0900

    6

--- sample3/main.mjs
+++ sample3/main.mjs
@@ -84,7 +84,7 @@ async function main() {
         resultText.textContent = `場所: ${first.Name}, 緯度: ${lct.lat}, 経度: ${lct.lng}`;
         // 地図を移動してラベル追加
         drawMap(map, lct);
-        const label = new Y.Label(new Y.LatLng(lct.lat, lct.lng),lct.lat lct.lng);
+        const label = new Y.Label(new Y.LatLng(lct.lat, lct.lng), lct.lat lct.lng);
         map.addFeature(label);
       } else {
         resultText.textContent = '正しい住所を入力してください';
